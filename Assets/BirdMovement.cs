fc9753f529c648501583f2da871bf198f038054b
NateSapoud <nathangdurocher@hotmail.com>
2022-06-08 19:38:55 -0400

    Jumping Mechanics

--- Assets/BirdMovement.cs
+++ Assets/BirdMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class BirdMovement : MonoBehaviour
 {
+    [SerializeField] Transform tBird;
+    [SerializeField] Rigidbody2D rbBird;
+    [SerializeField] int jumpHeight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        tBird = GetComponent<Transform>();
+        rbBird = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        
+    }
+
+
+    void Jump()
+    {
+
+        rbBird.velocity = new Vector2 (rbBird.velocity.x, jumpHeight);
+
     }
 }
